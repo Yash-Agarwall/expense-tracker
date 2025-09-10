8e6bb338cc5c7f7a78007d9cbb0910b41122c862
Yash Agarwal <yashagarwal7088@gmail.com>
2025-09-10 15:29:16 +0530

    Added filtering options, moved from basic CRUD to phase two, lets goooooo

--- src/TransactionPage.jsx
+++ src/TransactionPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import TransactionForm from "./TransactionForm";
 import TransactionList from "./TransactionList";
 
@@ -9,7 +9,9 @@ export default function TransactionPage() {
   });
 
   let [editingTransaction, setEditingTransaction] = useState(null);
-
+  let [filter, setFilter]= useState("all");
+  let [sort, setSort]=useState("newest");
+  
   let total = transactions.reduce(
     (totalExpense, spend) => totalExpense + spend.amount,
     0 
@@ -37,6 +39,26 @@ export default function TransactionPage() {
   useEffect(() => {
     localStorage.setItem("transactions", JSON.stringify(transactions));
   }, [transactions]);
+
+  let filteredTransactions = transactions.filter(tx=> {
+    //why to use ==== instead of ==
+    if(filter==="income") return tx.amount>0;
+    else if(filter==="expense") return tx.amount<0;
+    return true;
+  });
+
+  if(sort==="newest"){
+    filteredTransactions.sort((a,b)=> new Date(b.date) - new Date(a.date));
+  }
+  else if(sort==="oldest"){
+    filteredTransactions.sort((a,b)=> new Date(a.date) - new Date(b.date));
+  }
+  else if(sort==="high"){
+    filteredTransactions.sort((a,b)=> b.amount -a .amount);
+  }
+  else if(sort==="low"){
+    filteredTransactions.sort((a,b)=> a.amount - b.amount);
+  }
 
   return (
     <>
@@ -74,7 +96,23 @@ export default function TransactionPage() {
           
         }
       />
-      <TransactionList transactions={transactions} onDelete={handleDelete} onEdit={handleEdit}/>
+      <div>
+        <label htmlFor="filter">Filter:</label>
+        <select name="filter" id="filter" onChange={(e)=> setFilter(e.target.value)}>
+          <option value="all">All</option>
+          <option value="income">Income</option>
+          <option value="expense">Expense</option>
+        </select>
+
+        <label htmlFor="sort">Sort: </label>
+        <select name="sort" id="sort" onChange={(e)=> setSort(e.target.value)}>
+          <option value="newest">Date( Newest first )</option>
+          <option value="oldest">Date (Oldest first)</option>
+          <option value="high">Amount( High to low )</option>
+          <option value="low">Amount(Low to high)</option>
+        </select>
+      </div>
+      <TransactionList transactions={filteredTransactions} onDelete={handleDelete} onEdit={handleEdit}/>
       
     </>
   );
